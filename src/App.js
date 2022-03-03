f021589452965ce9e9d3854697a8e3577df3dbfc
Mithat Ercan <macbookpro@MacBookPro-MBP.home>
2022-03-03 23:54:05 +0300

    Return routes in layout component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
+import { useEffect } from "react";
+import { fetchMovies } from "./api/movieApi";
+import Layout from "./layouts";
+import Router from "./routes";
+
 function App() {
-  return 'Hello World'
+  useEffect(() => {
+    const fetchFunction = async () => {
+      const res = await fetchMovies();
+      console.log(res);
+    };
+
+    fetchFunction();
+  }, []);
+  return (
+    <Layout>
+      <Router />
+    </Layout>
+  );
 }
 
 export default App;
-
