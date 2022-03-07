a584dcd3984846f3b34ddbd110c4cde5d2811b01
Mithat Ercan <macbookpro@MacBookPro-MBP.home>
2022-03-08 01:04:08 +0300

    Update home view component and create its style

--- src/views/HomeView.js
+++ src/views/HomeView.js
@@ -1,7 +1,28 @@
-import React from "react";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getMovies } from "../redux/slicers/movieSlicer";
+import { v4 as uuid } from "uuid";
+import Slider from "../components/Slider";
+import Billboard from "../components/Billboard";
+import "./home.scss";
 
 const HomeView = () => {
-  return <div>HomeView</div>;
+  const dispatch = useDispatch();
+  const movies = useSelector((state) => state.movie.movies);
+
+  useEffect(() => {
+    dispatch(getMovies());
+  }, [dispatch]);
+
+  return (
+    <div>
+      <Billboard />
+      {movies &&
+        movies.map((movie) => (
+          <Slider key={uuid()} genre={movie.genre} movies={movie.movies} />
+        ))}
+    </div>
+  );
 };
 
 export default HomeView;
