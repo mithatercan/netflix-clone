fae8d199369ea6f4af1bb6a387da569cd78d9bdc
Mithat Ercan <macbookpro@MacBookPro-MBP.home>
2022-03-08 01:03:44 +0300

    Add layout component and its style

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,7 +1,14 @@
-import React from "react";
-
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import "./layout.scss";
 const Layout = ({ children }) => {
-  return <div>Layout{children}</div>;
+  return (
+    <div>
+      <Navbar />
+      <main>{children}</main>
+      <Footer />
+    </div>
+  );
 };
 
 export default Layout;
