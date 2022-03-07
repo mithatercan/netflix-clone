38ebbb684b800cfbcfcd86801b7ec7c8938d1d26
Mithat Ercan <macbookpro@MacBookPro-MBP.home>
2022-03-08 01:11:53 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,7 @@
-import { useEffect } from "react";
-import { fetchMovies } from "./api/movieApi";
 import Layout from "./layouts";
 import Router from "./routes";
 
 function App() {
-  useEffect(() => {
-    const fetchFunction = async () => {
-      const res = await fetchMovies();
-      console.log(res);
-    };
-
-    fetchFunction();
-  }, []);
   return (
     <Layout>
       <Router />
