184a79c37f29404a7f5a526c7b25a6b7e4baf18c
Mithat Ercan <mithat.ercan@hotmail.com>
2022-03-16 23:51:30 +0300

    Add slider buttons to slider component and update its style

--- src/components/Slider/index.js
+++ src/components/Slider/index.js
@@ -1,16 +1,21 @@
-import "./slider.scss";
+import React, { useRef } from "react";
+import SliderButtons from "./SliderButtons";
 import Box from "../Box";
+import "./slider.scss";
 
 const Slider = ({ genre, movies }) => {
+  const slider = useRef();
+
   return (
     <section className='slider-section-row'>
       <h3>{genre}</h3>
       <div className='slider-container'>
-        <div className='slider-wrapper df f-ai-c'>
+        <div ref={slider} className='slider-wrapper df f-ai-c'>
           {movies.map((movie, idx) => (
             <Box key={idx} movie={movie} />
           ))}
         </div>
+        <SliderButtons parent={slider} />
       </div>
     </section>
   );
