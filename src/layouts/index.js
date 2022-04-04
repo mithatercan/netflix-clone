c25de4efab7816df7cfa12d49d2f40d3db97a524
Mithat Ercan <mithat.ercan@hotmail.com>
2022-04-04 23:45:10 +0300

    Remove the navbar from login view

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,10 +1,13 @@
+import { useLocation } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import "./layout.scss";
+
 const Layout = ({ children }) => {
+  const { pathname } = useLocation();
   return (
     <div>
-      <Navbar />
+      {pathname !== "/login" && <Navbar />}
       <main>{children}</main>
       <Footer />
     </div>
