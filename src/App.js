e6ac58d59791310297ac7e880add3257c49181da
Mithat Ercan <mithat.ercan@hotmail.com>
2022-04-04 23:44:16 +0300

    Update the routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
-import Layout from "./layouts";
 import Router from "./routes";
 
 function App() {
-  return (
-    <Layout>
-      <Router />
-    </Layout>
-  );
+  return <Router />;
 }
 
 export default App;
--- src/routes/index.js
+++ src/routes/index.js
@@ -4,15 +4,20 @@ import {
   Route,
   Navigate,
 } from "react-router-dom";
+import Layout from "../layouts";
 import HomeView from "../views/Home";
+import LoginView from "../views/Login";
 
 const index = () => {
   return (
     <Router>
-      <Routes>
-        <Route path='/' element={<Navigate to='/browse' />} />
-        <Route path='/browse' element={<HomeView />} />
-      </Routes>
+      <Layout>
+        <Routes>
+          <Route path='/' element={<Navigate to='/login' />} />
+          <Route path='/login' element={<LoginView />} />
+          <Route path='/browse' element={<HomeView />} />
+        </Routes>
+      </Layout>
     </Router>
   );
 };
