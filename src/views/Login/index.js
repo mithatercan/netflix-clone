c3fe18e3eadbd9bc7bdeec209f0a13e40cde8db9
Mithat Ercan <mithat.ercan@hotmail.com>
2022-04-05 16:12:08 +0300

    Finish login view component

--- src/views/Login/index.js
+++ src/views/Login/index.js
@@ -1,11 +1,54 @@
-import React from "react";
+import { useNavigate } from "react-router-dom";
+import "./login-view.scss";
 
-const index = () => {
+const LoginView = () => {
+  const navigate = useNavigate();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    navigate("/browse");
+  };
   return (
-    <div>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla, velit.
+    <div className='login-view'>
+      <header>
+        <img src='logo.png' alt='logo' />
+      </header>
+      <div className='form-wrapper'>
+        <form onSubmit={handleSubmit}>
+          <h1>Sign in</h1>
+          <div className='form-group'>
+            <label htmlFor='email'>Email address</label>
+            <input
+              type='email'
+              className='form-control'
+              id='email'
+              aria-describedby='emailHelp'
+              placeholder='Enter email'
+            />
+          </div>
+          <div className='form-group'>
+            <label htmlFor='password'>Password</label>
+            <input
+              type='password'
+              className='form-control'
+              id='password'
+              placeholder='Password'
+            />
+          </div>
+          <input type='submit' value='Sign in' />
+
+          <div className='form-footer df f-fd-c'>
+            <p>
+              Don't have an account? <span>Sign up now</span>
+            </p>
+            <small>
+              This page is protected by Google reCAPTCHA to ensure you're not a
+              bot. Learn more.
+            </small>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
 
-export default index;
+export default LoginView;
